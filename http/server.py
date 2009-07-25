6e585e72ccb74a37b9ace2a24c222b4eea287671
Harish Mallipeddi <harishm@harishm-laptop.(none)>
2009-07-25 13:39:58 +0530

    Fixes for web.py 0.3 version

--- http/server.py
+++ http/server.py
@@ -23,7 +23,7 @@ urls = (
 class handle_task_new:
     def POST(self):
         i = web.input()
-        print task_new(force, 
+        return task_new(force, 
                 task_type = i.task_type,
                 task_id = i.task_id,
                 run_args = i.args,
@@ -32,17 +32,17 @@ class handle_task_new:
 
 class handle_task_status:
     def GET(self, id):
-        print task_status(force, id)
+        return task_status(force, id)
 
 class handle_task_results:
     def POST(self, id):
-        print task_results(force, id)
+        return task_results(force, id)
 
 
 def runserver(available_tasks, pool_size, address):
     global force
     force = TaskForce(available_tasks = available_tasks, pool_size = pool_size)
-    web.runsimple(web.webapi.wsgifunc(web.webpyfunc(urls, globals())), address)
+    web.runsimple(web.application(urls, globals()).wsgifunc(), address)
 
 class TaskforceDaemon(Daemon):
     def run(self, *args, **kwargs):
